b7c4b5ff25dd2366cd21462fb2be52f157744913
helmy16 <medohmada1623@gmail.com>
2022-10-03 02:01:50 +0200

    Updates

---
+++ precache-manifest.b474faa4bc537eee41ceae6aa3f0e6ad.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d5237fcdd4ed5b665f5591295ab6c7",
+    "url": "/mist/index.html"
+  },
+  {
+    "revision": "cd326ed942078edb3a3f",
+    "url": "/mist/static/css/2.30d90719.chunk.css"
+  },
+  {
+    "revision": "6bf52cc6cc8f0eb057e3",
+    "url": "/mist/static/css/main.c4951f32.chunk.css"
+  },
+  {
+    "revision": "cd326ed942078edb3a3f",
+    "url": "/mist/static/js/2.ed4d0c23.chunk.js"
+  },
+  {
+    "revision": "4505c94ec8fe387f205077ea7b884fed",
+    "url": "/mist/static/js/2.ed4d0c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf52cc6cc8f0eb057e3",
+    "url": "/mist/static/js/main.60877a2b.chunk.js"
+  },
+  {
+    "revision": "be9a17a4c61b6a8d1a66",
+    "url": "/mist/static/js/runtime-main.b4986dc4.js"
+  },
+  {
+    "revision": "721a4298ed35420c7b7b3554bab92a1a",
+    "url": "/mist/static/media/1.721a4298.mp4"
+  },
+  {
+    "revision": "69f3d863dc5111dcfe5104d89899d893",
+    "url": "/mist/static/media/icon.69f3d863.svg"
+  },
+  {
+    "revision": "54b0b4e7de85711c3796882b2b19eb00",
+    "url": "/mist/static/media/la-brands-400.54b0b4e7.woff2"
+  },
+  {
+    "revision": "9769d0a6a42cf73df12bc63a7a994131",
+    "url": "/mist/static/media/la-brands-400.9769d0a6.svg"
+  },
+  {
+    "revision": "a38ca9f0501109549cb659c1fe9ade65",
+    "url": "/mist/static/media/la-brands-400.a38ca9f0.eot"
+  },
+  {
+    "revision": "bbf83f8bb1039cd860051299d64ebcfd",
+    "url": "/mist/static/media/la-brands-400.bbf83f8b.ttf"
+  },
+  {
+    "revision": "fb598c9ccecd5fa1c6c769d0be60973b",
+    "url": "/mist/static/media/la-brands-400.fb598c9c.woff"
+  },
+  {
+    "revision": "2746742c09b070f74bd7d555e6b959fa",
+    "url": "/mist/static/media/la-regular-400.2746742c.eot"
+  },
+  {
+    "revision": "338f6f873b90c8045204f8ac52408166",
+    "url": "/mist/static/media/la-regular-400.338f6f87.woff"
+  },
+  {
+    "revision": "55c57a74937e6de260b67c62522f7ea1",
+    "url": "/mist/static/media/la-regular-400.55c57a74.svg"
+  },
+  {
+    "revision": "87dab6ff12ea107dafe1d52ec19c2ed8",
+    "url": "/mist/static/media/la-regular-400.87dab6ff.ttf"
+  },
+  {
+    "revision": "88d9d9416c58bde56378dc4439e3a144",
+    "url": "/mist/static/media/la-regular-400.88d9d941.woff2"
+  },
+  {
+    "revision": "36fc297902c9a2e857858baa6ac25f2c",
+    "url": "/mist/static/media/la-solid-900.36fc2979.woff2"
+  },
+  {
+    "revision": "79be4e9fcffc796ec3b2cb9a1f46d39e",
+    "url": "/mist/static/media/la-solid-900.79be4e9f.svg"
+  },
+  {
+    "revision": "87292218024ee1cab93406e228a0b7dd",
+    "url": "/mist/static/media/la-solid-900.87292218.woff"
+  },
+  {
+    "revision": "8c65fd3e9b53a609735fd6335fd05841",
+    "url": "/mist/static/media/la-solid-900.8c65fd3e.eot"
+  },
+  {
+    "revision": "bb49393b04bbf312a6cd055a051121d3",
+    "url": "/mist/static/media/la-solid-900.bb49393b.ttf"
+  },
+  {
+    "revision": "63478d3fe12229173b2d53a65a7c1e8a",
+    "url": "/mist/static/media/logo.63478d3f.png"
+  }
+]);
